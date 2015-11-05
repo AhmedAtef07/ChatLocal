d0ae5e2ba59d470ac22f4002bb243119e72573d3
Ahmed_Emad <7MADAH.1994@Gmail.com>
2015-11-06 01:41:13 +0200

    Reformat "GUIController" class methods

--- src/logic/GUIController.java
+++ src/logic/GUIController.java
@@ -11,12 +11,17 @@ import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUIController {
+
   private Stage stage;
   private ChatServer server;
   private ChatClient client;
+
+  private int width = 900;
+  private int height = 600;
 
   private TextArea chatArea;
 
@@ -27,17 +32,20 @@ public class GUIController {
   private void startGUI(Stage primaryStage) {
     stage = primaryStage;
     GridPane startGrid = getStartScreen();
-    Scene scene = new Scene(startGrid, 800, 500);
+    Scene scene = new Scene(startGrid, width, height);
     primaryStage.setScene(scene);
 
     //    primaryStage.setMaximized(true);
-    stage.centerOnScreen();
     stage.setTitle("Chat Room");
-    stage.setMaxHeight(800);
-    stage.setMaxWidth(500);
+//    stage.setMaxHeight(height);
+//    stage.setMaxWidth(width);
+    stage.setWidth(width);
+    stage.setHeight(height);
+    stage.centerOnScreen();
     stage.show();
+
     FlatterFX.style();
-    stage.requestFocus();
+    startGrid.requestFocus();
   }
 
   private GridPane getStartScreen() {
@@ -49,33 +57,42 @@ public class GUIController {
     grid.setPadding(new Insets(10));
 
     Label portLabel = new Label("Port #");
-    GridPane.setHalignment(portLabel, HPos.CENTER);
-    GridPane.setValignment(portLabel, VPos.CENTER);
-    portLabel.setAlignment(Pos.CENTER);
+//    GridPane.setHalignment(portLabel, HPos.CENTER);
+//    GridPane.setValignment(portLabel, VPos.CENTER);
+//    portLabel.setAlignment(Pos.CENTER);
     TextField portField = new TextField("7090");
+    GridPane.setHalignment(portField, HPos.CENTER);
+    GridPane.setValignment(portField, VPos.CENTER);
+    portField.setAlignment(Pos.CENTER);
+    VBox portBox = new VBox(5, portLabel, portField);
 
     Label ipAddressLabel = new Label("IP Address");
-    GridPane.setHalignment(ipAddressLabel, HPos.CENTER);
-    GridPane.setValignment(ipAddressLabel, VPos.CENTER);
-    ipAddressLabel.setAlignment(Pos.CENTER);
+//    GridPane.setHalignment(ipAddressLabel, HPos.CENTER);
+//    GridPane.setValignment(ipAddressLabel, VPos.CENTER);
+//    ipAddressLabel.setAlignment(Pos.CENTER);
     TextField ipAddressField = new TextField("192.168.1.");
+//    GridPane.setHalignment(ipAddressField, HPos.CENTER);
+//    GridPane.setValignment(ipAddressField, VPos.CENTER);
+//    ipAddressField.setAlignment(Pos.CENTER);
+    VBox ipAddresstBox = new VBox(5, ipAddressLabel, ipAddressField);
 
 
     Label usernameLabel = new Label("User Name");
-    GridPane.setHalignment(usernameLabel, HPos.CENTER);
-    GridPane.setValignment(usernameLabel, VPos.CENTER);
-    usernameLabel.setAlignment(Pos.CENTER);
+//    GridPane.setHalignment(usernameLabel, HPos.CENTER);
+//    GridPane.setValignment(usernameLabel, VPos.CENTER);
+//    usernameLabel.setAlignment(Pos.CENTER);
     TextField usernameField = new TextField("Atef & Emad");
+//    GridPane.setHalignment(usernameField, HPos.CENTER);
+//    GridPane.setValignment(usernameField, VPos.CENTER);
+//    usernameField.setAlignment(Pos.CENTER);
+    VBox usernameBox = new VBox(5, usernameLabel, usernameField);
 
     Button createButton = new Button("Create");
     Button joinButton = new Button("Join");
 
-    grid.add(portLabel, 0, 0, 4, 1);
-    grid.add(portField, 0, 1, 4, 1);
-    grid.add(ipAddressLabel, 0, 2, 4, 1);
-    grid.add(ipAddressField, 0, 3, 4, 1);
-    grid.add(usernameLabel, 0, 4, 4, 1);
-    grid.add(usernameField, 0, 5, 4, 1);
+    grid.add(portBox, 0, 0, 4, 2);
+    grid.add(ipAddresstBox, 0, 2, 4, 2);
+    grid.add(usernameBox, 0, 4, 4, 2);
     grid.add(createButton, 0, 6, 2, 1);
     grid.add(joinButton, 2, 6, 2, 1);
 
@@ -138,7 +155,7 @@ public class GUIController {
             "User 1", "User 2", "User 3", "User 4");
     list.setItems(items);
 
-    grid.add(list, 6, 0, 2, 4);
+    grid.add(list, 6, 0, 2, 5);
     grid.add(chatArea, 0, 0, 6, 4);
     grid.add(messageField, 0, 4, 4, 1);
     grid.add(sendButton, 4, 4, 2, 1);
@@ -148,15 +165,18 @@ public class GUIController {
 
   private void goToChat() {
     GridPane chatGrid = getChatScreen();
-    Scene scene = new Scene(chatGrid);
+    Scene scene = new Scene(chatGrid, width, height);
     stage.setScene(scene);
+//    stage.setMaxHeight(height);
+//    stage.setMaxWidth(width);
+    stage.setWidth(width);
+    stage.setHeight(height);
     stage.centerOnScreen();
-    stage.setMaxHeight(800);
-    stage.setMaxWidth(500);
     chatGrid.requestFocus();
   }
 
   public void appendMessage(String message) {
     chatArea.appendText(message + "\n");
   }
+
 }
